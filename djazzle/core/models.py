4cc7ccbd453be8680f0738a35258de29e429a95c
maurizio <maurizio.sambati@gmail.com>
2013-07-20 19:39:11 +0200

    api now creates the reversed file when saving

--- djazzle/core/models.py
+++ djazzle/core/models.py
@@ -5,3 +5,4 @@ class Track(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     data = models.FileField(upload_to='tracks/')
+    reversed_data = models.FileField(upload_to='reverse_tracks/', null=True, blank=True)
--- djazzle/core/views.py
+++ djazzle/core/views.py
@@ -1,7 +1,30 @@
+import os
+
+from django.core.files.temp import NamedTemporaryFile
+from django.core.files import File
+
 from rest_framework import viewsets
 
 from djazzle.core.models import Track
+from djazzle.core.utils import reverse_track
 
 
 class TrackViewSet(viewsets.ModelViewSet):
     model = Track
+
+    def post_save(self, obj, created=False):
+        super(TrackViewSet, self).post_save(obj, created)
+        assert obj.data
+
+        with NamedTemporaryFile(suffix='.mp3') as reversed_mp3:
+            filename = os.path.splitext(obj.data.name)[0]
+            obj.data.open()
+
+            try:
+                reverse_track(obj.data, reversed_mp3)
+            finally:
+                obj.data.close()
+
+            reversed_mp3.seek(0)
+            obj.reversed_data.save('%s_rev.mp3' % filename,
+                                   File(reversed_mp3), save=True)
