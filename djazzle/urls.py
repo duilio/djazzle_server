a605676d2210eae91a18eab0f41ff9b2820e108e
maurizio <maurizio.sambati@gmail.com>
2013-07-20 13:16:02 +0200

    add endpoint for admin website

--- djazzle/urls.py
+++ djazzle/urls.py
@@ -16,4 +16,5 @@ urlpatterns = patterns(
     '',
     url(r'^api/', include(router.urls)),
     url(r'^api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', include(admin.site.urls)),
 )
