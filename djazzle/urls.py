c5e6673dd33906b96efcc109072e47c7715eaa96
maurizio <maurizio.sambati@gmail.com>
2013-07-20 19:58:59 +0200

    add media file serving

--- djazzle/urls.py
+++ djazzle/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from rest_framework import routers
@@ -18,3 +20,9 @@ urlpatterns = patterns(
     url(r'^api-auth', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        # ... the rest of your URLconf goes here ...
+    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
